f95baf3e7bfe3f916d02968934e7ac1800fd885b
Saskianp <saskianovtarisaputri@gmail.com>
2024-11-23 00:37:01 +0700

    swagger

--- routes/module.routes.js
+++ routes/module.routes.js
@@ -26,17 +26,57 @@ router.delete('/content/delete/:id', authMiddleware.verifyToken, moduleControlle
  *     Module:
  *       type: object
  *       properties:
- *         name:
+ *         title:
  *           type: string
- *           description: name user
- *         email:
+ *           description: Judul modul
+ *         description:
  *           type: string
- *           description: email user
+ *           description: Deskripsi modul
+ *         class_module:
+ *           type: string
+ *           description: Kelas yang terdapat modul tersebut
+ *         date:
+ *           type: string
+ *           format: date
+ *           description: Tanggal modul
+ *         mentor_id:
+ *           type: integer
+ *           description: ID modul yang mengajar
  *       example:
- *         name: "Saskia"
- *         email: "saskia@gmail.com"
+ *         title: "Pemrograman Mobile"
+ *         description: "Mempelajari pemrograman berbasis mobile dengan Ionic & Angular"
+ *         class_module: "Pemrograman"
+ *         date: "2024-11-11"
+ *         mentor_id: 3
  */
-
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * /api/module/create:
+ *   post:
+ *     summary: Create a new module
+ *     tags: [Module]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Module'
+ *     responses:
+ *       201:
+ *         description: The module was successfully created
+ *       400:
+ *         description: Bad request, missing required data
+ *       500:
+ *         description: Some server error
+ */
 
 /**
  * @swagger
@@ -49,33 +89,13 @@ router.delete('/content/delete/:id', authMiddleware.verifyToken, moduleControlle
  * 
  * /api/module/:
  *    get:
- *     summary: Get mentor
+ *     summary: Get module
  *     tags: [Module]
  *     security:
  *       - bearerAuth: []
  *     responses:
  *       200:
- *         description: The mentor
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 id:
- *                   type: string
- *                   example: "12345"
- *                 name:
- *                   type: string
- *                   example: "Saskia"
- *                 email:
- *                   type: string
- *                   example: "saskia@gmail.com"
- *                 createdAt: 
- *                    type: date
- *                    example: "2024-11-22T10:17:54.000Z"
- *                 updatedAt: 
- *                    type: date
- *                    example: "2024-11-22T10:17:54.000Z"
+ *         description: The module
  *       404:
  *         description: The user was not found
  *       500:
@@ -94,7 +114,7 @@ router.delete('/content/delete/:id', authMiddleware.verifyToken, moduleControlle
  * 
  * /api/module/get/{id}:
  *    get:
- *     summary: Get mentor
+ *     summary: Get module
  *     tags: [Module]
  *     security:
  *       - bearerAuth: []
@@ -104,84 +124,15 @@ router.delete('/content/delete/:id', authMiddleware.verifyToken, moduleControlle
  *         schema:
  *           type: string
  *         required: true
- *         description: The mentor Id
+ *         description: The module Id
  *     responses:
  *       200:
- *         description: The mentor
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 id:
- *                   type: string
- *                   example: "12345"
- *                 name:
- *                   type: string
- *                   example: "Saskia"
- *                 email:
- *                   type: string
- *                   example: "saskia@gmail.com"
- *                 createdAt: 
- *                    type: date
- *                    example: "2024-11-22T10:17:54.000Z"
- *                 updatedAt: 
- *                    type: date
- *                    example: "2024-11-22T10:17:54.000Z"
+ *         description: The module
  *       404:
  *         description: The user was not found
  *       500:
  *         description: Some error happened
  */
-
-
-/**
- * @swagger
- *  components:
- *   securitySchemes:
- *     bearerAuth:
- *       type: http
- *       scheme: bearer
- *       bearerFormat: JWT
- * /api/module/create:
- *   post:
- *     summary: Create Mentor
- *     tags: [Module]
- *     security:
- *       - bearerAuth: []
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Module'
- *     responses:
- *       200:
- *         description: The user was successfully created
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 id:
- *                   type: string
- *                   example: "12345"
- *                 name:
- *                   type: string
- *                   example: "Saskia"
- *                 email:
- *                   type: string
- *                   example: "saskia@gmail.com"
- *                 createdAt: 
- *                    type: date
- *                    example: "2024-11-22T10:17:54.000Z"
- *                 updatedAt: 
- *                    type: date
- *                    example: "2024-11-22T10:17:54.000Z"
- *       500:
- *         description: Some server error
- */
-
 
 /**
  * @swagger
@@ -194,7 +145,7 @@ router.delete('/content/delete/:id', authMiddleware.verifyToken, moduleControlle
  * 
  * /api/module/update/{id}:
  *   put:
- *     summary: Update Mentor
+ *     summary: Update module
  *     tags: [Module]
  *     security:
  *       - bearerAuth: []
@@ -204,7 +155,7 @@ router.delete('/content/delete/:id', authMiddleware.verifyToken, moduleControlle
  *         schema:
  *           type: string
  *         required: true
- *         description: The mentor Id
+ *         description: The module Id
  *     requestBody:
  *       required: true
  *       content:
@@ -214,26 +165,6 @@ router.delete('/content/delete/:id', authMiddleware.verifyToken, moduleControlle
  *     responses:
  *       200:
  *         description: The user was successfully created
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 id:
- *                   type: string
- *                   example: "12345"
- *                 name:
- *                   type: string
- *                   example: "Saskia"
- *                 email:
- *                   type: string
- *                   example: "saskia@gmail.com"
- *                 createdAt: 
- *                    type: date
- *                    example: "2024-11-22T10:17:54.000Z"
- *                 updatedAt: 
- *                    type: date
- *                    example: "2024-11-22T10:17:54.000Z"
  *       500:
  *         description: Some server error
  */
@@ -249,7 +180,7 @@ router.delete('/content/delete/:id', authMiddleware.verifyToken, moduleControlle
  * 
  * /api/module/delete/{id}:
  *   delete:
- *     summary: Remove a mentor by Id
+ *     summary: Remove a module by Id
  *     tags: [Module]
  *     security:
  *       - bearerAuth: []
@@ -259,12 +190,187 @@ router.delete('/content/delete/:id', authMiddleware.verifyToken, moduleControlle
  *         schema:
  *           type: string
  *         required: true
- *         description: The mentor Id
+ *         description: The module Id
  *     responses:
  *       200:
- *         description: The mentor was deleted
+ *         description: The module was deleted
  *       404:
- *         description: The mentor was not found
+ *         description: The module was not found
+ *       500:
+ *         description: Some server error
+ */
+
+
+
+//Module Content
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Module-Content:
+ *       type: object
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: Judul materi modul
+ *         content:
+ *           type: string
+ *           description: Isi materi modul
+ *         module_id:
+ *           type: integer
+ *           description: ID modul yang mengajar
+ *       example:
+ *         title: "Konsep Ionic Angular"
+ *         content: "Mempelajari konsep awal Ionic & Angular"
+ *         module_id: 3
+ */
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * /api/module/content/create:
+ *   post:
+ *     summary: Create a new module content
+ *     tags: [Module-Content]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Module-Content'
+ *     responses:
+ *       201:
+ *         description: The module content was successfully created
+ *       400:
+ *         description: Bad request, missing required data
+ *       500:
+ *         description: Some server error
+ */
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * 
+ * /api/module/content/:
+ *    get:
+ *     summary: Get module content
+ *     tags: [Module-Content]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: The module content
+ *       404:
+ *         description: The user was not found
+ *       500:
+ *         description: Some error happened
+ */
+
+
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * 
+ * /api/module/content/get/{id}:
+ *    get:
+ *     summary: Get module content By module id
+ *     tags: [Module-Content]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The module content by module Id
+ *     responses:
+ *       200:
+ *         description: The module content
+ *       404:
+ *         description: The user was not found
+ *       500:
+ *         description: Some error happened
+ */
+
+/**
+ * @swagger
+ *  components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * 
+ * /api/module/content/update/{id}:
+ *   put:
+ *     summary: Update module content
+ *     tags: [Module-Content]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The module content Id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Module-Content'
+ *     responses:
+ *       200:
+ *         description: The user was successfully created
+ *       500:
+ *         description: Some server error
+ */
+
+/**
+ * @swagger
+ *  components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ * 
+ * /api/module/content/delete/{id}:
+ *   delete:
+ *     summary: Remove a module content by Id
+ *     tags: [Module-Content]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The module content Id
+ *     responses:
+ *       200:
+ *         description: The module content was deleted
+ *       404:
+ *         description: The module content was not found
  *       500:
  *         description: Some server error
  */
